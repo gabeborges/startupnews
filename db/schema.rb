48d8cd0774b545146a9d5d543626029edebc30f2
Gabriel Carpenedo <cbgabe@gmail.com>
2013-07-07 15:07:04 -0300

    Scaffold posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130707172339) do
+ActiveRecord::Schema.define(:version => 20130707174122) do
+
+  create_table "posts", :force => true do |t|
+    t.string   "title"
+    t.string   "url"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "posts", ["user_id"], :name => "index_posts_on_user_id"
 
   create_table "users", :force => true do |t|
     t.datetime "created_at", :null => false
